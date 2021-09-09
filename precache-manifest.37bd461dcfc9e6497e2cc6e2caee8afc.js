042b9ac5b9c5827ae0587754ec00e8fc406f293f
slickster23 <tokelo.foso23@gmail.com>
2021-09-09 02:19:55 +0200

    Updates

---
+++ precache-manifest.37bd461dcfc9e6497e2cc6e2caee8afc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097d261ea385fbd9811233a3b69725c8",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "de7a40f7e7d1809870c0",
+    "url": "/covid-19-tracker/static/css/2.15f857eb.chunk.css"
+  },
+  {
+    "revision": "6bb1bacc45f197c641e1",
+    "url": "/covid-19-tracker/static/css/main.be18c9aa.chunk.css"
+  },
+  {
+    "revision": "de7a40f7e7d1809870c0",
+    "url": "/covid-19-tracker/static/js/2.f0e233c9.chunk.js"
+  },
+  {
+    "revision": "ebe8559fffcb866c7cf4a61c8e60e51d",
+    "url": "/covid-19-tracker/static/js/2.f0e233c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bb1bacc45f197c641e1",
+    "url": "/covid-19-tracker/static/js/main.c906d23f.chunk.js"
+  },
+  {
+    "revision": "5bdbd92167a555c8ec71",
+    "url": "/covid-19-tracker/static/js/runtime-main.81159ed7.js"
+  }
+]);
